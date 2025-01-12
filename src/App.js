f3bde0c15204aa88390fa5f14e1fbe249ba0e91b
angelromero <angel.romero@fwpr.com>
2025-01-12 11:39:51 -0400

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Homescreen';
+import Navbar from './Pages/Home/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <Home />
-
+        <Router>
+          <div>
+            <Navbar />
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+
+function Navbar() {
+
+    const [navActive, setNavActive] = useState(false);
+
+    const toggleNav = () => {
+        setNavActive(!navActive);
+    }
+
+    const closeMenu = () => {
+        setNavActive(false);
+    }
+
+    useEffect(() => {   
+       const handleResize = () => {
+            if (window.innerWidth <= 500) {
+                closeMenu();
+            }
+       }
+       window.addEventListener('resize', handleResize);
+
+       return () => {
+        window.removeEventListener('resize', handleResize);
+       }
+    }, []);
+
+    useEffect(() => {
+        if(window.innerWidth <= 1200){
+            closeMenu();
+        }
+
+    }, []);
+
+    return (
+        <nav className={`navbar ${navActive ? "active" : ""}`}>
+            <div>
+                <img src="./img/logo.svg" alt="logo" />
+                <a className={`nav_hamburguer ${navActive ? "active" : ""}`} onClick={toggleNav}></a>
+                <span className="hamburger_line"></span>
+                <span className="hamburger_line"></span>
+                <span className="hamburger_line"></span>
+            </div>
+            <div className={`navbar-items ${navActive ? "active" : ""}`}>
+                <ul>
+                    <li>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="HeroSection" className="navbar-content">Home</Link>
+                    </li>
+                    <li>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="MyPortfolio" className="navbar-content">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="AboutMe" className="navbar-content">About Me</Link>
+                    </li>
+                </ul>
+            </div>
+             <div>
+                <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="Contact" className="btn btn-outline-primary">Contact Me</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
+ 
